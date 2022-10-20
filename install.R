4b7ca82511cc8fce4f67ad8826a4ff276d56c3a8
Leslie Coonrod <coonrod@uoregon.edu>
2022-10-20 15:10:50 -0700

    update installs

--- install.R
+++ install.R
@@ -1,8 +1,8 @@
 install.packages("tidyverse")
 install.packages("rmarkdown")
-install.packages("httr")
-install.packages("shinydashboard")
-install.packages('leaflet')
+#install.packages("httr")
+#install.packages("shinydashboard")
+#install.packages('leaflet')
 
 BiocManager::install("DESeq2")
 BiocManager::install("apeglm")
