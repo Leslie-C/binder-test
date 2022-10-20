f1a842d034d72d351e59b8684ea831e3f0d279a8
Leslie Coonrod <coonrod@uoregon.edu>
2022-10-20 12:03:23 -0700

    add data and update ipynb

--- install.R
+++ install.R
@@ -3,3 +3,6 @@ install.packages("rmarkdown")
 install.packages("httr")
 install.packages("shinydashboard")
 install.packages('leaflet')
+
+BiocManager::install("DESeq2")
+BiocManager::install("apeglm")
